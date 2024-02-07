9a49fff5fa8cfbd5c4bff43c7cd5532266f9c648
MarkDanilchenko <my.ucheba.2023@gmail.com>
2024-02-07 18:55:52 +0300

    Add bootstrap, favicon and custom scss

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { createApp } from 'vue';
 import App from '@/App.vue';
 import router from '@/router/router.js';
 import store from '@/store/index.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import '@/assets/scss/styles.scss';
 
 // custom UI components from src/components/UI/
 import UIcomponents from '@/components/UI/index.js';
